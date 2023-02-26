2b3132a2b0891cc795a28aa363e259f28bbc89ca
Nwafor Emmanuel <emmanuelnwaforce@gmail.com>
2023-02-26 19:48:46 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import EmojiData from './emoji.json';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      search: "",
+      data: []
+    };
+  }
+
+  componentDidMount() {
+    this.updateData();
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    if (prevState.search !== this.state.search) {
+      this.updateData();
+    }
+  }
+
+  updateData() {
+    const newData = EmojiData.filter(emoji => emoji.title.toLowerCase().includes(this.state.search.toLowerCase()));
+    this.setState({ data: newData });
+  }
+
+  render() {
+    return (
+      <div>
+        <center>
+          <h1>🤗Emoji Search🤗</h1>
+          <input size="30" type="text" value={this.state.search} onChange={(e) => this.setState({ search: e.target.value })} />
+        </center>
+        {this.state.data.map(emoji => (
+          <div className="card" key={emoji.title}>
+            <div className="card-body" onClick={() => {navigator.clipboard.writeText(emoji.symbol);alert("Emoji Copy")}}>
+              {emoji.symbol} {emoji.title}
+            </div>
+          </div>
+        ))}
+      </div>
+    );
+  }
 }
 
 export default App;
